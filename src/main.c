c9b5ac5ce1b90636069005cc6a6b665c47eb607e
antonioasaro <antonioasaro@users.noreply.github.com>
2013-10-05 08:16:20 -0700

    Added mario icon for watch menu

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@
 
 #define MY_UUID { 0xB6, 0xA0, 0xAB, 0x5F, 0x92, 0xB7, 0x4C, 0x2B, 0xBC, 0x0F, 0x34, 0x6B, 0x99, 0xAE, 0x30, 0xA0 }
 PBL_APP_INFO(MY_UUID,
-             "Mario", "Denis Dzyubenko",
+             "Antonio Mario", "Denis Dzyubenko",
              1, 0, /* App version */
              RESOURCE_ID_IMAGE_MENU_ICON,
              APP_INFO_WATCH_FACE);
